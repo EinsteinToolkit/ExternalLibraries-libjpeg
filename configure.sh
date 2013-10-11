f5f18895729275cc02838452aaa073b43fba9a48
knarf <knarf@ba58616d-3abc-4c77-8745-3ba8ad4a582f>
2013-10-11 00:43:28 +0000

    add lib/arm-linux-gnueabihf to searchpath
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/libjpeg/trunk@36 ba58616d-3abc-4c77-8745-3ba8ad4a582f

--- configure.sh
+++ configure.sh
@@ -26,7 +26,7 @@ if [ -z "${LIBJPEG_DIR}" ]; then
         # libraries might have different file extensions
         for libext in a so dylib; do
             # libraries can be in /lib or /lib64
-            for libdir in lib64 lib/x86_64-linux-gnu lib lib/i386-linux-gnu; do
+            for libdir in lib64 lib/x86_64-linux-gnu lib lib/i386-linux-gnu lib/arm-linux-gnueabihf; do
                 FILES="include/jpeglib.h $libdir/libjpeg.$libext"
                 # assume this is the one and check all needed files
                 LIBJPEG_DIR="$dir"
